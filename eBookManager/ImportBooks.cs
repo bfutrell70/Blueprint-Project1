4eedcf1ab6d53a11d56cbf5644a4648cd2f8fb29
Brian Futrell <bfutrel@gmail.com>
2018-05-06 17:40:36 -0400

    added enums and hashset for allowed extensions

--- eBookManager/ImportBooks.cs
+++ eBookManager/ImportBooks.cs
@@ -20,6 +20,11 @@ namespace eBookManager
         private List<StorageSpace> spaces;
         private enum StorageSpaceSelection { New = -9999, NoSelection = -1}
 
+        private HashSet<string> AllowedExtension => new
+            HashSet<string>(StringComparer.InvariantCultureIgnoreCase)
+            {".doc", ".docx", ".pdf", ".epub" };
+        private enum Extention { doc = 0, docx = 1, pdf = 2, epub = 3}
+
         public ImportBooks()
         {
             InitializeComponent();
